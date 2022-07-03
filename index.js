c8403d05bfdf6b69e9d10c3027c9d157ba8f1806
Enrique Vergara <36646104+kiikechavez@users.noreply.github.com>
2022-07-03 22:31:14 +0000

    product new added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import {User} from './User.js'
+import {Product} from './Product.js'
 
 const enrique = new User('Enrique', 'Vergara')
 console.log(enrique)
+console.log(new Product('laptop', 1500))
